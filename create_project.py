fbd9ed6c7d78593754af446a06974aa8523f2a51
MaruKazeMaru <kaze47111@gmail.com>
2023-09-25 18:08:46 +0900

    make create_project.py more readable

--- create_project.py
+++ create_project.py
@@ -13,6 +13,7 @@ def create_db(db_path:str, motion_num:int):
     conn = sqlite3.connect(db_path)
     cur = conn.cursor()
 
+    # CREATE TABLES
     cur.execute('PRAGMA foreign_keys=true')
     cur.execute( \
         'CREATE TABLE motion(' \
@@ -42,22 +43,33 @@ def create_db(db_path:str, motion_num:int):
         ')' \
     )
 
-    for i in range(motion_num):
-        cur.execute('INSERT INTO motion(id) values(%d)' % i)
+    # INSERT motion
+    values = ['(%d)' % i for i in range(motion_num)]
+    cur.execute('INSERT INTO motion(id) VALUES' + ','.join(values))
 
+    # INSERT tr_prob
+    values = [] 
     init_prob = 1.0 / float(motion_num)
     for i in range(motion_num):
         for j in range(motion_num):
-            cur.execute('INSERT INTO tr_prob(from_motion, to_motion, elem) values(%d, %d, %f)' % (i, j, init_prob))
+            values.append('(%d, %d, %f)' % (i, j, init_prob))
+    cur.execute('INSERT INTO tr_prob(from_motion, to_motion, elem) VALUES' + ','.join(values))
 
+    # INSERT heatmap_user
+    values = []
     v = np.array([1.0, 0.0])
     ang = 2.0 * np.pi / float(motion_num)
     c = np.cos(ang)
     s = np.sin(ang)
     R = np.array([[c,-s], [s,c]])
     for i in range(motion_num):
-        cur.execute('INSERT INTO heatmap_user(motion, avr_x, avr_y, inv_var_xx, inv_var_xy, inv_var_yy) values(%d, %f, %f, %f, %f, %f)' % (i, v[0], v[1], 1.0, 0.0, 1.0))
+        values.append('(%d, %f, %f, %f, %f, %f)' % (i, v[0], v[1], 1.0, 0.0, 1.0))
         v = R @ v
+    sql = \
+    'INSERT INTO heatmap_user' \
+    ' (motion, avr_x, avr_y, inv_var_xx, inv_var_xy, inv_var_yy)' \
+    ' VALUES'
+    cur.execute(sql + ','.join(values))
 
     conn.commit()
     conn.close()
