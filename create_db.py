b910a1342444f51fed503fa246f20690299e8ec5
MaruKazeMaru <kaze47111@gmail.com>
2023-09-25 17:27:20 +0900

    make create_db.py able to create heatmap_user table

--- create_db.py
+++ create_db.py
@@ -4,14 +4,13 @@
 # SPDX-License-Identifier: MIT License
 
 import os
+import sys
 import argparse
 import sqlite3
+import numpy as np
 
-def main(args):
-    if os.path.exists(args.dbpath):
-        return
-
-    conn = sqlite3.connect(args.dbpath)
+def create_db(db_path:str, motion_num:int):
+    conn = sqlite3.connect(db_path)
     cur = conn.cursor()
 
     cur.execute('PRAGMA foreign_keys=true')
@@ -30,20 +29,57 @@ def main(args):
         'FOREIGN KEY(to_motion) REFERENCES motion(id)' \
         ')' \
     )
-    for i in range(args.motionnum):
+    cur.execute( \
+        'CREATE TABLE heatmap_user(' \
+        'id INTEGER PRIMARY KEY AUTOINCREMENT,' \
+        'motion INTEGER,' \
+        'avr_x REAL,' \
+        'avr_y REAL,' \
+        'inv_var_xx REAL,' \
+        'inv_var_xy REAL,' \
+        'inv_var_yy REAL,' \
+        'FOREIGN KEY(motion) REFERENCES motion(id)' \
+        ')' \
+    )
+
+    for i in range(motion_num):
         cur.execute('INSERT INTO motion(id) values(%d)' % i)
-    init_prob = 1.0 / float(args.motionnum)
-    for i in range(args.motionnum):
-        for j in range(args.motionnum):
+
+    init_prob = 1.0 / float(motion_num)
+    for i in range(motion_num):
+        for j in range(motion_num):
             cur.execute('INSERT INTO tr_prob(from_motion, to_motion, elem) values(%d, %d, %f)' % (i, j, init_prob))
+
+    v = np.array([1.0, 0.0])
+    ang = 2.0 * np.pi / float(motion_num)
+    c = np.cos(ang)
+    s = np.sin(ang)
+    R = np.array([[c,-s], [s,c]])
+    for i in range(motion_num):
+        cur.execute('INSERT INTO heatmap_user(motion, avr_x, avr_y, inv_var_xx, inv_var_xy, inv_var_yy) values(%d, %f, %f, %f, %f, %f)' % (i, v[0], v[1], 1.0, 0.0, 1.0))
+        v = R @ v
 
     conn.commit()
     conn.close()
 
 
+def main(args):
+    dir_path = '/var/lib/wili/' + args.projectname
+    if os.path.exists(dir_path):
+        print('%s already exists' % args.projectname, file=sys.stderr)
+        exit(1)
+
+    os.mkdir(dir_path)
+
+    db_path = dir_path + '/db.sqlite3'
+    with open(db_path, 'x'):
+        pass
+    create_db(db_path, args.motionnum)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="WILI db initializer")
     parser.add_argument("motionnum", help="int. number of motion", type=int)
-    parser.add_argument("dbpath", help="str. path of database(SQLite)", type=str)
+    parser.add_argument("projectname", help="str. name of project", type=str)
     args = parser.parse_args()
     main(args)
